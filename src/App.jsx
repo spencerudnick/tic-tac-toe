bb6115ce43852eb1ba4e7609bd4a1dc138fcea65
Spencer Rudnick <spencer@synth.kitchen>
2020-02-10 23:18:51 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    gameBoard: [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ]
+  };
+
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+export default App;
